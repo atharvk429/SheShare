7bffcffa116d771be383844ddcdf393798dd4095
atharvk429 <atharvkulkarni429@gmail.com>
2024-05-25 20:28:20 +0530

    ACAO

--- api/index.js
+++ api/index.js
@@ -27,6 +27,13 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://main--sheshare0.netlify.app');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+  });  
+
 // app.use(cors({
 //     origin: ['http://localhost:5173','https://main--sheshare0.netlify.app'],
 //     credentials: true
